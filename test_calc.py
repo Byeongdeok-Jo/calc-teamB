a98adaf56aebbe9d31e8a2e0c9ce3dabd335934d
car <znargon@gmail.com>
2024-07-10 16:48:41 +0900

    [feature] add minus method

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
 from calc import Calc
 
+
 class TestCalc(TestCase):
-    pass
+
+    def test_get_minus(self):
+        calc = Calc()
+        self.assertEqual(0, calc.get_minus(5, 5))
+        self.assertEqual(2, calc.get_minus(5, 3))
+        self.assertEqual(-1, calc.get_minus(5, 6))
