4f1456aa9f665362df65498631f8c727046086c9
jeonghun <jhe.kim@samsung.com>
2024-07-10 16:57:01 +0900

    Add divide feature

--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
 class Calc:
     def get_minus(self, num1, num2):
         return num1 - num2
+
+    def get_divided_value(self, n1, n2):
+        if n2 == 0:
+            return None
+
+        return n1 / n2
--- test_calc.py
+++ test_calc.py
@@ -9,3 +9,9 @@ class TestCalc(TestCase):
         self.assertEqual(0, calc.get_minus(5, 5))
         self.assertEqual(2, calc.get_minus(5, 3))
         self.assertEqual(-1, calc.get_minus(5, 6))
+
+    def test_get_divided_value(self):
+        sut = Calc()
+        expected = sut.get_divided_value(5, 5)
+
+        self.assertEqual(expected, 1)
