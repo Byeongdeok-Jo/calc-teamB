b27c49b31c21b52958cad7a9e43d158c90ead8b8
jihun75.kim <gnswlrla0218@naver.com>
2024-07-10 16:49:38 +0900

    update test_get_gom

--- test_calc.py
+++ test_calc.py
@@ -2,4 +2,8 @@ from unittest import TestCase
 from calc import Calc
 
 class TestCalc(TestCase):
-    pass
+    def test_get_gom(self):
+        sut = Calc()
+        self.assertEqual(sut.get_gom(2, 3), 6)
+
+
